55014633bc32a1da656d0478be276c10e9518446
JorgeLaRosa <jorgem.larosa@gmail.com>
2022-05-01 22:54:11 -0300

    PruebaFer

---
+++ src/app/students/students.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { StudentsRoutingModule } from './students-routing.module';
+import { SharedModule } from '../shared/shared.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    StudentsRoutingModule
+  ]
+})
+export class StudentsModule { }
